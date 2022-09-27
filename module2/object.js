8cedf6dacd904a5d78259b3ced70c1458aff0525
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-09-27 21:33:07 +0530

    Object in js

---
+++ module2/object.js
@@ -0,0 +1,11 @@
+let obj ={}
+console.log(obj);
+
+let objShyam = {
+    Name : "Shyam Sundar Sahoo",
+    Age : 37,
+    MobileNo : 9337757671,
+    "LastName" : "Sahoo"
+}
+
+console.log(objShyam);
