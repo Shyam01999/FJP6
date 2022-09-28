37a1c9fbb8e4359e3e0b530d9513ff77a2f1fd16
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-09-28 22:07:53 +0530

    child process

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,4 +1,7 @@
 let cp = require('child_process');
 // console.log(cp);
 
-cp.execFileSync("calc");
+// cp.execFileSync("calc");
+
+let content = cp.execSync("node test.js");
+console.log("Output of test.js file: "+content);
