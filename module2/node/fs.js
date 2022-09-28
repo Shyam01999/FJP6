4d40c61dc7e9c75affbc175908dc44923cb10c8f
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-09-28 23:33:52 +0530

    file System in node js

---
+++ module2/node/fs.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+//console.log(fs);
+
+let path = require('path');
+
+let filePath = path.join(__dirname,"file.txt");
+// console.log(filePath);
+
+fs.writeFileSync(filePath,"Again writing on text file")
