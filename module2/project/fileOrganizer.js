a5bad4b7592e6f477a4cfdd08c0f6c20b366c10e
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-10-04 22:55:11 +0530

    fileOrganiser project

--- module2/project/fileOrganizer.js
+++ module2/project/fileOrganizer.js
@@ -1,5 +1,21 @@
-let inputArr = process.argv;
-console.log(inputArr);
-//['pathToNode','pathToFile','inputElement']
-let input = inputArr[2];
-console.log(input);
+// let inputArr = process.argv;
+// console.log(inputArr);
+// //['pathToNode','pathToFile','inputElement']
+// let input = inputArr[2];
+// console.log(input);
+
+let fs = require('fs');
+
+let folderKaPath = process.argv[2];
+//console.log(folderKaPath);
+
+let folderExists = fs.existsSync(folderKaPath)
+
+if(folderExists){
+    //we will code
+    //console.log("Path is valid");
+    let files = fs.readdirSync(folderKaPath);
+    console.log(files);
+}else{
+    console.log("Path is not valid")
+}
