03812660d2d457bf709bfdbcaf7f0f715780162a
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-11-03 22:37:41 +0530

    webscrapping

--- module2/web_scrapping/activity.js
+++ module2/web_scrapping/activity.js
@@ -2,7 +2,7 @@ const request = require('request');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard";
+const link = "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2022-23-1298134/bangladesh-vs-india-35th-match-group-2-1298169/full-scorecard";
 
 request('link', cb);
 
@@ -11,11 +11,12 @@ function cb(error, response, html ){
         console.error('error:', error); // Print the error if one occurred
     }
     else{
-        const dom = new JSDOM(html);
-        const document = dom.window.document;
-        let teamName = document.querySelectorAll(".match-info.match-info-MATCH.match-info-MATCH-half-width .teams .name");
-        console.log(teamName[0].textContent);
-        console.log(teamName[1].textContent);
+        // const dom = new JSDOM(html);
+        // const document = dom.window.document;
+        // let teamName = document.querySelectorAll("ds-text-tight-l ds-font-bold ds-text-ui-typo hover:ds-text-ui-typo-primary ds-block ds-truncate");
+        // console.log(teamName[0].textContent);
+        // console.log(teamName[1].textContent);
+        console.log(html);
 
     }
 }
