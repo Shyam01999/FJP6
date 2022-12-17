aca57fa1b3dfa11479ad9621f65781d6eb6cf124
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2022-12-18 00:59:25 +0530

    jira functionality added and ui change

---
+++ module3/jira/js/script.js
@@ -0,0 +1,88 @@
+let addBtn = document.querySelector(".add-btn");
+let modal = document.querySelector(".toolbox-main-cont");
+let textCont = document.querySelector(".textarea-cont");
+let mainCont = document.querySelector(".main-cont");
+let priortyColor = document.querySelectorAll(".priorty-color");
+let removeBtn = document.querySelector(".remove-btn");
+let ticket = document.querySelectorAll(".ticket-cont");
+let addModal = true;
+let color = ["lightpink","lightblue","lightgreen","lightblack"];
+let modalPriortyColor = color[color.length-1];
+let addBtnColor = true;
+
+
+addBtn.addEventListener("click", () => {
+    if(addModal){ 
+        modal.style.display = "flex";
+    }else{
+        modal.style.display = "none";
+    }
+    addModal = !addModal;  
+})
+
+removeBtn.addEventListener('click', ()=>{
+    console.log("remove btn clicked");
+    if(addBtnColor){
+        removeBtn.style.color = "red"; 
+    }
+    else{
+        removeBtn.style.color = "black"
+    }
+    addBtnColor = !addBtnColor
+})
+
+for(let i=0; i<priortyColor.length; i++){
+    let allPriortyColor = priortyColor[i];
+    allPriortyColor.addEventListener('click', ()=>{
+        for(let j=0; j<priortyColor.length; j++){
+            priortyColor[j].classList.remove('active')
+        }
+        allPriortyColor.classList.add('active');
+        modalPriortyColor = allPriortyColor.classList[1];
+        console.log(modalPriortyColor);
+    })
+}
+
+modal.addEventListener("keydown",(e)=>{
+    let key = e.key;
+    if(key == "Enter"){
+        createTicket(modalPriortyColor, textCont.value);
+        textCont.value =" ";
+        modal.style.display = "none";
+        addModal = !addModal;
+    }
+})
+
+createTicket = (ticketColor,task)=>{
+let ticketCont = document.createElement("div");
+ticketCont.setAttribute("class","ticket-cont");
+ticketCont.innerHTML = `<div class="ticket-color ${ticketColor}"></div>
+                 <div class="ticket-id">#edhjdk</div>
+                 <div class="task-area">${task}</div>`
+mainCont.appendChild(ticketCont);
+//handle delete
+ticketCont.addEventListener('click',()=>{
+    if(!addBtnColor){
+        ticketCont.remove();
+    }
+})
+
+//handle color
+let ticketBandColor = ticketCont.querySelector(".ticket-color");
+ ticketBandColor.addEventListener('click',function(){
+    let currentTicketColor = ticketBandColor.classList[1];
+    let currentTicketColorIdx = -1;
+    for(let i=0; i<color.length; i++){
+        if(currentTicketColor == color[i]){
+            currentTicketColorIdx = i;
+            break;
+        }
+        
+    }
+    let nextTicketColorIdx = (currentTicketColorIdx+1)%color.length;
+    let nextTicketColor = color[nextTicketColorIdx];
+    ticketBandColor.classList.remove(currentTicketColor);
+    ticketBandColor.classList.add(nextTicketColor);
+
+ })
+}
