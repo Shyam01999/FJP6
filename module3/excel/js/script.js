6c092ada53cf27de08e837f4b72da8d390fc6b91
Shyam01999 <shyamsundarsahoo1998@gmail.com>
2023-01-03 22:36:12 +0530

    db init

--- module3/excel/js/script.js
+++ module3/excel/js/script.js
@@ -1,37 +1,3 @@
-let cellsContentDiv = document.querySelector(".cells-content");
-
-function initCells() {
-    let cellsContent = "<div class='top-left-cell'></div>";
-
-    //top row
-    cellsContent+="<div class='top-row'>"
-        for(let i=0; i<26; i++){
-        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
-        }
-    cellsContent+="</div>"
-
-    //left col
-    cellsContent+="<div class='left-col'>"
-        for(let i=0; i<100; i++){
-            cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
-        }
-    cellsContent+="</div>"
-
-    cellsContent+="<div class='cells'>"
-    for (let i = 0; i < 100; i++) {
-        cellsContent += "<div class='row'>";
-        //columns
-        for (let j = 0; j < 26; j++) {
-            cellsContent += "<div class='cell' contentEditable>CELLS</div>";
-        }
-        cellsContent+="</div>";
-    }
-    cellsContent+="</div>"
-
-    cellsContentDiv.innerHTML = cellsContent;
-}
-
-initCells()
 
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
@@ -46,3 +12,24 @@ cellsContentDiv.addEventListener("scroll",function(e){
     topLeftCell.style.top = scrollFromTop+"px";
     topLeftCell.style.left = scrollFromLeft+"px";
 })
+
+let db ;
+function initDb(){
+    db =[];
+    for(let i=0; i<100; i++){
+        let row = [];
+        for(let j=0; j<26; j++){
+            let name = String.fromCharCode(65+j)+(i+1)+"";
+            let cellObject = {
+                name:name,
+                value:""
+            }
+            row.push(cellObject);
+        }
+        db.push(row);
+    }
+
+}
+initDb();
+
+console.log(db);
