05123c934fa24dca46d72b805c894430c0be3b67
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-01-12 23:51:34 +0530

    routing added and page created

---
+++ pern_series/client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <header>
+      <div className="container">
+        <div className="logo-brand">
+          <NavLink to="/"><h1>Shop kart</h1></NavLink>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="services">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="contact">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="register">Register</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
