d49dfd4e2a06705aa334ca01f9089beb3da52ecf
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-01-13 19:15:49 +0530

    register page started

--- pern_series/client/src/pages/Register.jsx
+++ pern_series/client/src/pages/Register.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
 function Register() {
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+  }
   return (
-    <h1>This is Register page</h1>  
+    <section>
+      <main>
+        <div className='section-registration'>
+          <div className='container grid grid-two-cols'>
+            <div className='registration-image'>
+              <img src="./images/register.jpg" alt="registration image" width="500" height="500"/>
+            </div>
+            <div className='registration-form'>
+              <h1 className='main-heading mb-3'>registration form</h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username">username</label>
+                  <input type="text" name='username' placeholder='username' id='username' required autoComplete='off'/>
+                </div>
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input type="email" name='email' placeholder='email' id='email' required autoComplete='off'/>
+                </div>
+                <div>
+                  <label htmlFor="password">password</label>
+                  <input type="password" name='password' placeholder='password' id='password' required autoComplete='off'/>
+                </div>
+                <div>
+                  <label htmlFor="contact number">contact number</label>
+                  <input type="number" name='contact number' placeholder='contact number' id='contact number' required autoComplete='off'/>
+                </div>
+                <br />
+                <button type='submit' className='btn btn-submit'>Register Now</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section> 
     )
 }
 
