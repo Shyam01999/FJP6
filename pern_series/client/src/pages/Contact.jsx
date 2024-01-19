975ff2c4e0b3bb98924c67c2323d1ca04ac4f3a7
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-01-19 07:39:26 +0530

    contact page design created and data store in state

--- pern_series/client/src/pages/Contact.jsx
+++ pern_series/client/src/pages/Contact.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Contact() {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  //Handle submit to send data
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // alert("Under construction");
+    // if (
+    //   data.username == "" ||
+    //   data.email == "" ||
+    //   data.contactnumber == "" ||
+    //   data.password == ""
+    // ) {
+    //   alert("You have empty feild");
+    // } else {
+    try {
+      // console.log("data", data);
+      // Assuming 'yourApiEndpoint' is the actual endpoint to send data to
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/login",
+        data,
+        { withCredentials: true }
+      );
+      // console.log("res", response);
+      if (response.data.message == "Login Successful") {
+        localStorage.setItem("token", response.data.token);
+        alert(response.data.message);
+        setData({
+          email: "",
+          password: "",
+        });
+        navigate("/");
+      } else {
+        // The request failed
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(`Registration page error ${error}`);
+    }
+    // }
+  };
+
   return (
-    <h1>This is Contact page</h1>
-  )
+    <section>
+      <main>
+        <div className="section-registration">
+          <div className="container grid grid-two-cols">
+            <div className="registration-image">
+              <img
+                src="./images/contact.jpg"
+                alt="contact form image"
+                width="500"
+                height="500"
+              />
+            </div>
+            <div className="registration-form">
+              <h1 className="main-heading mb-3">
+                <span className="highlight-text">contact f</span>orm
+              </h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username">username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={data.username}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="message">message</label>
+                  <textarea
+                    name="message"
+                    id="message"
+                    value={data.message}
+                    onChange={handleChange}
+                    rows="6"
+                    cols="30"
+                    required
+                  ></textarea>
+                </div>
+                <br />
+                <button type="submit" className="btn btn-submit">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
 }
 
 export default Contact;
