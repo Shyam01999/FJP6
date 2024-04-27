d99c548f450d490f1d2998bc8958f9b5f9be9bc7
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-04-27 22:03:11 +0530

    order all order and status api added

--- Ecommerce/server/controllers/order/order.js
+++ Ecommerce/server/controllers/order/order.js
@@ -1,5 +1,6 @@
 const db = require("../../models/index");
 const Order = db.Order;
+const Product = db.Product;
 
 //new order
 const newOrder = async (req, res) => {
@@ -38,7 +39,7 @@ const getSingleOrder = async (req, res) => {
         const { id } = req.params;
         const order = await Order.findByPk(id);
         if (!order) {
-            res.status(404).json({ message: `Order not found with id ${id}.` })
+            return res.status(404).json({ message: `Order not found with id ${id}.` })
         }
 
         res.status(200).json({ success: true, order });
@@ -54,10 +55,10 @@ const getSingleOrder = async (req, res) => {
 const myOrderDetails = async (req, res) => {
     try {
         const orders = await Order.findAll({ where: { user: req.user.id } });
-        
+
 
         if (!orders) {
-            res.status(404).json({ message: `Order not found with this user.` })
+            return res.status(404).json({ message: `Order not found with this user.` })
         }
 
         res.status(200).json({ success: true, orders });
@@ -69,19 +70,88 @@ const myOrderDetails = async (req, res) => {
     }
 }
 
+//get all order --admin
+const getAllOrder = async (req, res) => {
+    try {
+        const orders = await Order.findAll();
+        if (!orders) {
+            return res.status(404).json({ message: `No order has been placed` })
+        }
+
+        const totalAmount = orders.reduce((totalprice, curr) => {
+            return totalprice = totalprice + curr.totalPrice;
+        }, 0);
+
+        res.status(200).json({ success: true, orders, totalAmount });
+    }
+    catch (error) {
+        console.log('Error in fetching order details:', error);
+        res.status(500).json({ error: "Internal Server Error" })
+
+    }
+}
+
+//update order status
+const updateOrderStatus = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const order = await Order.findByPk(id);
+        let isOutOfStock = false;
+
+        if (!order) {
+            return res.status(404).json({ message: `No order found with this id ${id}` })
+        }
+        else if (order.orderStatus == "Delivered") {
+            
+            return res.status(403).json({ message: "You have has already delivered this order" })
+        }
+        else {
+            if(order.orderStatus == "Processing")
+            for (const item of order.orderItems) {
+                const product = await Product.findByPk(item.productid);
+                if (!product || product.stock <= 0) {
+                    isOutOfStock = true;
+                    return res.status(403).json({ message: `Product ${item.name} is out of stock` });
+                }
+                await updateStock(item.productid, item.quantity);
+            }
+
+            if (!isOutOfStock) {
+                order.orderStatus = req.body.status;
+
+                if (req.body.status === "Delivered") {
+                    order.deliveredAt = Date.now();
+                }
+
+                await order.save();
+                return res.status(200).json({ success: true, message: `Order has been ${order.orderStatus} successfully` });
+            }
+
+        }
+
+
+    }
+    catch (error) {
+        console.log('Error in updaating order details:', error);
+        res.status(500).json({ error: "Internal Server Error" })
+
+    }
+}
+
+
 const deleteOrder = async (req, res) => {
     try {
         const order = await Order.findOne({ where: { id: req.params.id } });
-        
+
 
         if (!order) {
-            res.status(404).json({ message: `Order not found with this id ${req.params.id}.` })
+            return res.status(404).json({ message: `Order not found with this id ${req.params.id}.` })
         }
 
         // Delete the order
         await order.destroy();
 
-        res.status(200).json({ message: `Order with ID ${req.params.id} deleted successfully`});
+        res.status(200).json({ message: `Order with ID ${req.params.id} deleted successfully` });
 
     }
     catch (error) {
@@ -90,9 +160,20 @@ const deleteOrder = async (req, res) => {
     }
 }
 
+async function updateStock(id, quantity) {
+    const product = await Product.findByPk(id);
+
+    product.stock = Number(product.stock) - Number(quantity);
+
+    await product.save()
+}
+
 module.exports = {
     newOrder,
     getSingleOrder,
     myOrderDetails,
-    deleteOrder
+    getAllOrder,
+    updateOrderStatus,
+    deleteOrder,
+
 };
