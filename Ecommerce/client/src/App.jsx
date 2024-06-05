f66d08d5e3fbefed2541c1200aff0b40c24bf5b2
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-06-05 22:54:23 +0530

    update

--- Ecommerce/client/src/App.jsx
+++ Ecommerce/client/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Button} from '@mui/material';
 import './App.css'
+import "./styles/layout.css";
+import "./styles/module.css";
+import "./styles/theme.css";
+import "./styles/states.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NotFound from './components/NotFound/NotFound';
+import { useEffect } from 'react';
+import { notifySuccess } from './constant/toastAlerts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    notifySuccess("Welcome to Shop Ease App")
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+     <Button variant="contained">Hello world</Button>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter> 
     </>
   )
 }
